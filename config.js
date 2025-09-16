647c4855a21c1f3efe7039916813c55779f82c06
Manthan Bhosale <cryptomb2006@gmail.com>
2025-09-16 11:02:00 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDBPuvBZ9c993SHAKBNADsQmDUHJEzMfCo",
+    authDomain: "jhchat-372c2.firebaseapp.com",
+    projectId: "jhchat-372c2",
+    storageBucket: "jhchat-372c2.firebasestorage.app",
+    messagingSenderId: "583049279700",
+    appId: "1:583049279700:web:72d1d3be7f56f3c22ae341",
+    measurementId: "G-75EYL8W0SW"
+  };
---
+++ function.js
@@ -0,0 +1,14 @@
+export function getMsg() {
+    firebase.firestore().collection('jh-Chat').onSnapshot(changes => {
+        changes.docChanges().forEach((changes) =>{
+            if(changes.type == 'added'){
+                let pTag = document.createElement('p');
+                pTag.innerText = `message : ${changes.doc.data().message}`;
+
+                let chatContainer = document.getElementById('chat');
+                chatContainer.appendChild(pTag);
+
+            }
+        })
+    })
+}
