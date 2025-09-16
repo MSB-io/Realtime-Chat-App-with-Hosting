edd6ca0592f3714231a47e79334b7cdd1238a10d
Manthan Bhosale <cryptomb2006@gmail.com>
2025-09-16 11:05:46 +0530

    Refactor code formatting for consistency and readability in config.js, function.js, and jh-Chat.html

--- function.js
+++ function.js
@@ -1,14 +1,16 @@
 export function getMsg() {
-    firebase.firestore().collection('jh-Chat').onSnapshot(changes => {
-        changes.docChanges().forEach((changes) =>{
-            if(changes.type == 'added'){
-                let pTag = document.createElement('p');
-                pTag.innerText = `message : ${changes.doc.data().message}`;
+  firebase
+    .firestore()
+    .collection("jh-Chat")
+    .onSnapshot((changes) => {
+      changes.docChanges().forEach((changes) => {
+        if (changes.type == "added") {
+          let pTag = document.createElement("p");
+          pTag.innerText = `message : ${changes.doc.data().message}`;
 
-                let chatContainer = document.getElementById('chat');
-                chatContainer.appendChild(pTag);
-
-            }
-        })
-    })
+          let chatContainer = document.getElementById("chat");
+          chatContainer.appendChild(pTag);
+        }
+      });
+    });
 }
